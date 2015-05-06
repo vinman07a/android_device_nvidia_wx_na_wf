64f2c59776492c764605357aebef88abdc09d486
vinman07a <vinman07a@gmail.com>
2015-05-06 12:43:16 -0400

    bring up to SMAICP

--- wx_na_wf.mk
+++ wx_na_wf.mk
@@ -10,5 +10,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="nvidia/wx_na_wf/shieldtablet:5.0.1/LRX22C/29082_493.9700:user/release-keys" \
     PRIVATE_BUILD_DESC="wx_na_wf-user 5.0.1 LRX22C 29082_493.9700 release-keys"
 
+# SaberMod
+TARGET_SM_AND := 4.9
+TARGET_ARCH := arm
+TARGET_SM_KERNEL := 4.8
+ENABLE_PTHREAD := true
+O3_OPTIMIZATIONS := true
+
 PRODUCT_NAME := cm_wx_na_wf
 PRODUCT_DEVICE := wx_na_wf
