5b0762b737e3b31acec97055fe26e9ee29a084d6
vinman07a <vinman07a@gmail.com>
2015-05-07 08:27:37 -0400

    update makefile for AICP

--- wx_na_wf.mk
+++ wx_na_wf.mk
@@ -3,7 +3,7 @@
 $(call inherit-product, device/nvidia/wx_na_wf/full_wx_na_wf.mk)
 
 # Inherit some common Tablet stuff.
-$(call inherit-product, vendor/aicp/config/common_tablet.mk)
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=wx_na_wf \
@@ -11,12 +11,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="nvidia/wx_na_wf/shieldtablet:5.0.1/LRX22C/29082_493.9700:user/release-keys" \
     PRIVATE_BUILD_DESC="wx_na_wf-user 5.0.1 LRX22C 29082_493.9700 release-keys"
 
-# SaberMod
-TARGET_SM_AND := 4.9
-TARGET_ARCH := arm
-TARGET_SM_KERNEL := 4.8
-ENABLE_PTHREAD := true
-O3_OPTIMIZATIONS := true
-
-PRODUCT_NAME := cm_wx_na_wf
+PRODUCT_NAME := aicp_wx_na_wf
 PRODUCT_DEVICE := wx_na_wf
